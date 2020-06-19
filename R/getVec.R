a540a91c723c21ac69e399306dcf7bdf5c34c71b
khuggler <khuggler@uwyo.edu>
2020-06-19 16:18:30 -0700

    getVec

--- R/getVec.R
+++ R/getVec.R
@@ -1,5 +1,4 @@
 #' @title Vectronic Collar Data Combine
-#
 #' @description This function combines the csv files downloaded by vectronic GPS Plus X
 #' @param vecpath Path your Vectronic GPS Plus X Program saves CSV files to
 #' @param tzone time zone that GPS data should be converted to. "MST", "US/Pacific"
@@ -17,27 +16,27 @@
 #' \donttest{getVec(vecpath='/home/mhayes1/Desktop/Testing/')}
 #'
 getVec<-function(vecpath, tzone){
-  
+
   llist<-list.files(vecpath,
                     pattern='GPS_Default.csv',full.names=T)
-  
+
   vdat<-data.frame()
   for(i in 1:length(llist)){
     sub<-read.csv(llist[i],stringsAsFactors = F,encoding='latin1')
     vdat<-rbind(vdat,sub)
   }
-  
+
   vdat<-vdat[,c(2,3,4,16,17,48,13,14)]
   vdat$TelemDate<-as.POSIXct(paste(vdat$UTC_Date,vdat$UTC_Time,sep=' '),'%m/%d/%Y %I:%M:%S %p',tz='UTC')
-  
+
   attributes(vdat$TelemDate)$tzone<-tzone
   vdat<-vdat[,c(1,9,4,5,6,7,8)]
   names(vdat)<-c('CollarSerialNumber','TelemDate','HDOP','X2D.3D','Temperature','Lat','Long')
   vdat<-vdat[complete.cases(vdat$Long),]
   vdat<-vdat[complete.cases(vdat$Lat),]
-  
+
   sp::coordinates(vdat)<-~Long+Lat
   sp::proj4string(vdat)<-'+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'
-  
+
   return(vdat)
 }
