a7bc9087f2d7fa1b4af85e12b294b45cfdee4b31
khuggler <khuggler@uwyo.edu>
2021-01-04 16:08:45 -0800

    add online scraping of vectronics gps data to function and removed reliance on gps plus X

--- R/sheep.gps.R
+++ R/sheep.gps.R
@@ -1,6 +1,6 @@
 #' @title sheep.gps
-#' @description Merge Raw GPS Data with AnimalID, Sex, and Eartag ID
-#' @param vecpath path where vec data is located
+#' @description Scrape GPS data  from online server, and merge with downloaded data from collars
+#' @param keys full path to one or more key files
 #' @param sheepdb path to sheep capture database
 #' @param tzone desired time zone of gps data: "MST" or "US/Pacific"
 #' @param capcol name of column where capture/start date exists
@@ -15,119 +15,126 @@
 #' @examples
 #'
 
-sheep.gps<-function(vecpath, sheepdb, tzone, capcol, dateformat, mortcol, dnld.data, dnld.fold, extracols){
-  sheep.dat<-Ovis::getVec(vecpath, tzone)
+
+sheep.gps<-function(keys, sheepdb, tzone, capcol, dateformat, mortcol, dnld.data, dnld.fold, extracols){
+  
+  devtools::install_github("Huh/collar")
+
+  key_path <- get_paths(keys)
+  sheep.dat<-collar::fetch_vectronics(key_path, type = "gps")
+  sheep.dat<-data.frame(sheep.dat)
+  sheep.dat$acquisitiontime<-as.POSIXct(sheep.dat$acquisitiontime, format = paste0("%Y-%m-%d", "T", "%H:%M:%S"), tz = "UTC")
+  
   sheep.db<-read.csv(sheepdb, stringsAsFactors = F)
   newdb<-sheep.db
-
-
-
+  
+  
   # transform dates
   sheep.db$CapDate<-as.Date(sheep.db[, capcol], format = dateformat)
-
+  
   sheep.db$MortDate<-as.Date(sheep.db[, mortcol], format = dateformat)
   sheep.db$MortDate<-ifelse(is.na(sheep.db$MortDate), as.character(Sys.Date()), as.character(sheep.db$MortDate))
   sheep.db$MortDate<-as.Date(sheep.db$MortDate, tryFormats = c('%Y-%m-%d', "%m/%d/%Y"))
-
-  sheep.dat$Date<-as.character(strptime(sheep.dat$TelemDate, format = "%Y-%m-%d"))
+  
+  sheep.dat$Date<-as.character(strptime(sheep.dat$acquisitiontime, format = "%Y-%m-%d"))
   sheep.dat$Date<-as.Date(sheep.dat$Date, tryFormats = c('%Y-%m-%d', "%m/%d/%Y"))
+  
 
-  #make a dataframe of gps data
-  sheep.dat<-data.frame(sheep.dat)
-  sheep.dat$Date<-strftime(sheep.dat$TelemDate, format = "%Y-%m-%d")
-
-
-  sheep.dat<-sheep.dat[, c(1:8)]
-
+  #sheep.dat$Date<-strftime(sheep.dat$TelemDate, format = "%Y-%m-%d")
+  
+  sheep.dat<-sheep.dat[, c(1:4, 6:13,41:44,47)]
+  
   if(dnld.data == TRUE){
     files<-list.files(dnld.fold, full.names = T)
-
+    
     all.dnld<-data.frame()
-
+    
     for(i in 1:length(files)){
       sub<-read.csv(files[[i]], stringsAsFactors = F)
       #aid<-sapply(strsplit(as.character(files[[i]]), "\\.csv"), "[", 1)
       #aid<-sapply(strsplit(as.character(aid), "_GPS"), "[", 1)
       #aid<-sapply(strsplit(as.character(aid), "/"), "[", 7)
       #sub$AID<-aid
-
+      
       sub$TelemDate<-paste(sub$UTC_Date, sub$UTC_Time, sep = " ")
-
-      names<-c('CollarID', 'TelemDate', 'DOP', "X3D_Error..m.", 'Temp...C.', 'Latitude....', 'Longitude....', 'UTC_Date')
+      
+      names<-c('CollarID', 'TelemDate', 'DOP', 'Temp...C.', 'Latitude....', 'Longitude....', 'UTC_Date')
       sub<-sub[,names]
-
+      
+      # fix order of columns
+      
+      sheep.dat<-sheep.dat[, c('idcollar', 'acquisitiontime', 'dop', 'temperature', 'latitude', 'longitude', 'Date')]
+      
       names(sub)<-names(sheep.dat)
-
-      sub$TelemDate<-as.POSIXct(sub$TelemDate, format = "%m/%d/%Y %I:%M:%S %p", tz = "UTC")
-      sub$TelemDate<-format(sub$TelemDate, tz = "US/Pacific", usetz = FALSE)
-      sub$TelemDate<-as.POSIXct(sub$TelemDate, format = "%Y-%m-%d %H:%M:%S")
-
-      sub<-sub[with(sub, order(-CollarSerialNumber, TelemDate)),]
-
-
-
+      
+      
+      sub$acquisitiontime<-as.POSIXct(sub$acquisitiontime, format = "%m/%d/%Y %I:%M:%S %p", tz = "UTC")
+      sub$acquisitiontime<-format(sub$acquisitiontime, tz = "US/Pacific", usetz = FALSE)
+      sub$acquisitiontime<-as.POSIXct(sub$acquisitiontime, format = "%Y-%m-%d %H:%M:%S")
+      
+      sub<-sub[with(sub, order(-idcollar, acquisitiontime)),]
+      
+      
+      
       all.dnld<-rbind(sub, all.dnld)
-
-
+      
+      
     }
 
-
-
-    sheep.dat$X2D.3D<-rep(all.dnld$X2D.3D, length.out = nrow(sheep.dat))
-
+    #sheep.dat$X2D.3D<-rep(all.dnld$X2D.3D, length.out = nrow(sheep.dat))
+    
     all.dnld$Date<-as.Date(all.dnld$Date, format = "%m/%d/%Y")
     sheep.dat$Date<-as.Date(sheep.dat$Date, format = "%Y-%m-%d")
-
+    
     sheep.dat<-rbind(sheep.dat, all.dnld)
-    sheep.dat<-sheep.dat[with(sheep.dat, order(-CollarSerialNumber, TelemDate)),]
-
-
-    sheep.dat$hr<-strftime(sheep.dat$TelemDate, format = "%H")
+    sheep.dat<-sheep.dat[with(sheep.dat, order(-idcollar, acquisitiontime)),]
+    
+    
+    sheep.dat$hr<-strftime(sheep.dat$acquisitiontime, format = "%H")
     sheep.dat$id<-paste(sheep.dat$Date, sheep.dat$hr, sep = "_")
-
-
-    sheep.dat<-sheep.dat[!duplicated(sheep.dat[, c('CollarSerialNumber', 'id')]),]
-
-
-  }
-
-
+    
+    
+    sheep.dat<-sheep.dat[!duplicated(sheep.dat[, c('idcollar', 'id')]),]
+    
+    
+  }    
+  
   col.hist<-Ovis::collar.history(data = newdb, idcol = "AID", study = "washington", capdate = "CapDate", mortdate = "MortDate")
-
+  
   uni<-unique(col.hist$AnimalID)
-
+  
   #final.sheep<-data.frame()
-
+  
   for(k in 1:nrow(col.hist)){
     xx<-col.hist[k, ]
     print(k)
-
+    
     for(l in 1:2){
       if(l == 1){
         xxx<-xx[, c('AnimalID', 'Serial1', 'Serial1Start', 'Serial1End')]
       }
-
+      
       if(l == 2){
         xxx<-xx[, c('AnimalID', 'Serial2', 'Serial2Start', 'Serial2End')]
       }
-
+      
       if(is.na(xxx[1,2])){next}
-      ss<-sheep.dat[sheep.dat$CollarSerialNumber==xxx[1,2],]
+      ss<-sheep.dat[sheep.dat$idcollar==xxx[1,2],]
       ss<-ss[(ss$Date> xxx[,3])&ss$Date < (xxx[,4]),]
-      ss<-ss[complete.cases(ss$Lat),]
-
+      ss<-ss[complete.cases(ss$latitude),]
+      
       if(nrow(ss)==0){next}
       if(nrow(ss)>0){
         ss$AID<-xxx[1,1]
       }
-
+      
       if(l == 1){
         ald<-ss
       }
       if(l>1){
         ald<-rbind(ald,ss)
       }
-
+      
     }
     if(k == 1){
       outsp<-ald
@@ -136,39 +143,40 @@ sheep.gps<-function(vecpath, sheepdb, tzone, capcol, dateformat, mortcol, dnld.d
       outsp<-rbind(outsp,ald)
     }
   }
-
+  
   outsp<-data.frame(outsp)
   outsp2<-outsp[!duplicated(outsp[,1:4]),]
-  outsp2<-outsp2[complete.cases(outsp2$CollarSerialNumber),]
+  outsp2<-outsp2[complete.cases(outsp2$idcollar),]
 
-
-
-
-if(!is.na(extracols)){
-  uni<-unique(outsp2$AID)
-  final<-data.frame()
-  for(i in 1:length(uni)){
-    sub.sheep<-outsp2[outsp2$AID == uni[i],]
-    sub.db<-sheep.db[sheep.db$AID == uni[i],]
-    sub.db<-sub.db[nrow(sub.db),]
-
-    for(l in 1:length(extracols)){
-      sub.sheep[,extracols[l]]<-sub.db[,extracols[l]][1]
-
+  
+  
+  
+  if(!is.na(extracols)){
+    uni<-unique(outsp2$AID)
+    final<-data.frame()
+    for(i in 1:length(uni)){
+      sub.sheep<-outsp2[outsp2$AID == uni[i],]
+      sub.db<-sheep.db[sheep.db$AID == uni[i],]
+      sub.db<-sub.db[nrow(sub.db),]
+      
+      for(l in 1:length(extracols)){
+        sub.sheep[,extracols[l]]<-sub.db[,extracols[l]][1]
+        
+      }
+      
+      final<-rbind(sub.sheep, final)
     }
-
-    final<-rbind(sub.sheep, final)
+    
+    return(final)
+    
   }
-
-  return(final)
-
-}
-
-if(is.na(extracols)){
-  return(outsp2)
-}
-
+  
+  if(is.na(extracols)){
+    return(outsp2)
   }
-
-
-
+  
+}
+    
+  
+    
+  
