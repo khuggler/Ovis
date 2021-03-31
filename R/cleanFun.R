5d03a9824aa48c082421c746f247950b18d28c43
Katey Sue Huggler <khuggler@uwyo.edu>
2021-03-31 11:02:07 -0700

    cleanFun

--- R/cleanFun.R
+++ R/cleanFun.R
@@ -3,7 +3,7 @@
 #' @description Cleans output from GStar or Irid. Cleaning is user-defined and
 #' based on either hdop or 2d/3d fix status. Outputs three results in list format: Cleaned Data,
 #' "Bad" data and a report. Data is also cleaned if the 'dist' (from as.ltraj) is greater than the 98 percentile.
-#' @param data SpatialPointsDataFrame as outputed from GStar or Irid. 
+#' @param data SpatialPointsDataFrame as outputed from GStar or Irid.
 #' @param cval Cutoff for 2d/3d fix status. Valid values are 2 or 3. 3 will only use 3d fixes while 2 uses 2 and 3.
 #' @param hval Cutoff for hdop quantile cutoff. Default is 10 which is equalt to 90th quantile.
 #' @param filename Full path to store cleaning report file.
@@ -28,7 +28,7 @@
 cleanFun<-function (data, projectedproj, fixstat = "X2D.3D", hdopC = "HDOP", cval = 3,
                     hval = 10, filename){
 
-  if (type == "irid") {
+
     rawDat <- data
     #rawDat<-rawDat[rawDat@coords[,1]<(-50),]
     data<-rawDat
@@ -37,28 +37,28 @@ cleanFun<-function (data, projectedproj, fixstat = "X2D.3D", hdopC = "HDOP", cva
     id$chk<-paste(id$IdCol,id$TelemDate,sep='_')
     id<-id[!duplicated(id$chk),]
     id<-id[,-(ncol(id))]
-    
-    
-    
+
+
+
     t <- Ovis::trajfun(id, "TelemDate", "Easting", "Northing",
                        "IdCol")
-    
-    
+
+
     id <- Ovis::bindfun(t, id)
-    
+
     stepquants <- as.numeric(quantile(id$dist, na.rm = T,
                                       seq(0, 1, 0.01))[100])
-    
+
     ddr <- id[which(id$dist > stepquants),
                 ]
-    
+
     id <- id[which(id[, "dist"] <= stepquants), ]
     data <- id
-    
-    
+
+
     sp::coordinates(data) <- ~Easting + Northing
     sp::proj4string(data) <- projectedproj
-    
+
     if('HDOP' %in% names(data)){
     val <- as.numeric(quantile(data@data$HDOP, na.rm = T, probs = seq(0,
                                                                       1, 0.1))[hval])
@@ -67,18 +67,18 @@ cleanFun<-function (data, projectedproj, fixstat = "X2D.3D", hdopC = "HDOP", cva
     if(!'HDOP' %in% names(data)){
       hdr<-data.frame()
     }
-    
-    
+
+
     if(fixstat %in% names(data)){
     fxr <- data[which(data@data[, fixstat] < cval), ]
     }
     if(!fixstat %in% names(data)){
       fxr <- data.frame()
     }
-    
-    
+
+
     bdata<-nrow(hdr) + nrow(fxr) + nrow(ddr)
-    
+
     report <- list(c("Number Bad Locs", "HDOP Clean Val",
                      "Fix Status Clean Val", "Points Removed VIA HDOP",
                      "Points Removed VIA FixStatus", "Points Removed Step Length (greater than 99% quantile)"),
@@ -87,9 +87,9 @@ cleanFun<-function (data, projectedproj, fixstat = "X2D.3D", hdopC = "HDOP", cva
     report <- as.data.frame(do.call("rbind", report), stringsAsFactors = F)
     names(report) <- as.character(report[1, ])
     report <- report[2, ]
-    
+
     write.table(report, filename, sep = ",", row.names = F)
     return(list(data, report))
-  }
-  
+
+
  }
